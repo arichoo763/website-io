22b8fb23e2b0ab66508447409e6008929c9431bf
arichoo763 <arichoo763@gmail.com>
2025-09-15 00:17:54 +0700

    Update gambar dan link

--- src/app/(view)/page.tsx
+++ src/app/(view)/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className='max-w-lg mx-auto bg-gradient-to-b from-slate-500/10 to-transparent'>
       <HeaderInfo />
-      <div className='flex flex-col w-full gap-1 my-4 px-6 md:-mt-8 relative z-10'>
+      <div className='flex flex-col w-full gap-1 my-4 px-6 relative z-10'>
         {
           datalinks.map((item, index) => (
             <LinkItem {...item} key={index} />
--- src/data/links.ts
+++ src/data/links.ts
@@ -1,77 +1,87 @@
 export type TDatalinks = {
-  url: string
-  label: string
-  logo: string
-}
+  url: string;
+  label: string;
+  logo: string;
+};
 export const datalinks: TDatalinks[] = [
   {
-    url: 'https://app.nodepay.ai/register?ref=qNGIdJekhAZDf3l',
-    label: 'Nodepay',
-    logo:'/images/projects/nodepay.png'
+    url: "https://app.nodepay.ai/register?ref=qNGIdJekhAZDf3l",
+    label: "Nodepay",
+    logo: "/images/projects/nodepay.png",
   },
   {
-    url: 'https://app.gradient.network/signup?code=0RALV7',
-    label: 'gradient',
-    logo:'/images/projects/nodepay.png'
+    url: "https://app.gradient.network/signup?code=0RALV7",
+    label: "gradient",
+    logo: "/images/projects/gradient.png",
   },
   {
-    url: 'https://app.blockmesh.xyz/register?invite_code=02250c06-4c6b-40b9-87dd-a69ea4cfdea6',
-    label: 'Blockmesh/Perception',
-    logo:'/images/projects/nodepay.png'
+    url: "https://app.blockmesh.xyz/register?invite_code=02250c06-4c6b-40b9-87dd-a69ea4cfdea6",
+    label: "Blockmesh/Perception",
+    logo: "/images/projects/blockmash.png",
   },
   {
-    url: 'https://app.nodepay.ai/register?ref=qNGIdJekhAZDf3l',
-    label: 'Nodepay',
-    logo:'/images/projects/nodepay.png'
+    url: "https://zero.kaisar.io/register?ref=FpilSU409",
+    label: "Kaisar",
+    logo: "/images/projects/kaisar.png",
   },
   {
-    url: 'https://app.nodepay.ai/register?ref=qNGIdJekhAZDf3l',
-    label: 'Nodepay',
-    logo:'/images/projects/nodepay.png'
+    url: "https://app.aigaea.net/register?ref=ga9KDm8SBwJsvg",
+    label: "Aigaea",
+    logo: "/images/projects/aigaea.png",
   },
   {
-    url: 'https://app.nodepay.ai/register?ref=qNGIdJekhAZDf3l',
-    label: 'Nodepay',
-    logo:'/images/projects/nodepay.png'
+    url: "https://app.minionlab.ai/index?referralCode=RmLOtCwl",
+    label: "Minionlab",
+    logo: "/images/projects/minionlab.png",
   },
   {
-    url: 'https://app.nodepay.ai/register?ref=qNGIdJekhAZDf3l',
-    label: 'Nodepay',
-    logo:'/images/projects/nodepay.png'
+    url: "https://app.despeed.net/register?ref=sFEApWvGOAqs",
+    label: "Despeed",
+    logo: "/images/projects/despeed.png",
   },
   {
-    url: 'https://app.nodepay.ai/register?ref=qNGIdJekhAZDf3l',
-    label: 'Nodepay',
-    logo:'/images/projects/nodepay.png'
+    url: "https://toggle.pro/sign-up/4e7953c9-5517-4504-9039-14b93acd436d",
+    label: "Toogle",
+    logo: "/images/projects/toogle.png",
   },
   {
-    url: 'https://app.nodepay.ai/register?ref=qNGIdJekhAZDf3l',
-    label: 'Nodepay',
-    logo:'/images/projects/nodepay.png'
+    url: "https://dashboard.teneo.pro/auth/signup?referralCode=m20oa",
+    label: "Teneo",
+    logo: "/images/projects/teneo.png",
   },
   {
-    url: 'https://app.nodepay.ai/register?ref=qNGIdJekhAZDf3l',
-    label: 'Nodepay',
-    logo:'/images/projects/nodepay.png'
+    url: "https://app.chakra.dev/?ref=ZB92ZX",
+    label: "Chakra",
+    logo: "/images/projects/chakra.png",
   },
   {
-    url: 'https://app.nodepay.ai/register?ref=qNGIdJekhAZDf3l',
-    label: 'Nodepay',
-    logo:'/images/projects/nodepay.png'
+    url: "https://testnet.pharosnetwork.xyz/experience?inviteCode=C5jTMQlfqBblQuE0",
+    label: "Pharos Network",
+    logo: "/images/projects/pharos.png",
   },
   {
-    url: 'https://app.nodepay.ai/register?ref=qNGIdJekhAZDf3l',
-    label: 'Nodepay',
-    logo:'/images/projects/nodepay.png'
+    url: "https://app.grass.io/register?referral=_5xYOis-b6rZ0zN",
+    label: "Grass",
+    logo: "/images/projects/grass.png",
   },
   {
-    url: 'https://app.nodepay.ai/register?ref=qNGIdJekhAZDf3l',
-    label: 'Nodepay',
-    logo:'/images/projects/nodepay.png'
+    url: "https://t.me/UXUYbot/app?startapp=A_6809184891_inviteEarn",
+    label: "Uxuy Wallet",
+    logo: "/images/projects/uxuy.png",
   },
   {
-    url: 'https://app.nodepay.ai/register?ref=qNGIdJekhAZDf3l',
-    label: 'Nodepay',
-    logo:'/images/projects/nodepay.png'
+    url: "https://marina-protocol.com/link/referral?code=WEIRDCLEVER",
+    label: "Marina Protocol",
+    logo: "/images/projects/marina.png",
   },
-]
+  {
+    url: "https://miranetwork.io",
+    label: "Mira Network",
+    logo: "/images/projects/marine.png",
+  },
+  {
+    url: "https://sparkchain.ai/register/?r=46593155",
+    label: "Spark Chain",
+    logo: "/images/projects/spark.png",
+  },
+];
