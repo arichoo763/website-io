e1af0eff6c3c994a301f8e9dd0d6c7d31c043aa0
arichoo763 <arichoo763@gmail.com>
2025-09-24 18:31:36 +0700

    Update Header

--- src/app/components/header-info.tsx
+++ src/app/components/header-info.tsx
@@ -4,17 +4,11 @@ import React from 'react'
 export default function HeaderInfo() {
   return (
     <div className='pt-[80px] relative min-h-[35vh]'>
-      <Image className='opacity-80' src={'/images/header-2.png'} fill alt='mas io' />
+      <Image className='opacity-90' src={'/images/header-2.png'} fill alt='mas io' />
       <div className="absolute top-0 bottom-0 right-0 left-0 flex flex-col items-center justify-center pt-12">
         <Image className='w-20 h-20 border rounded-full' src={'/images/rio.png'} width={100} height={100} alt='mas io' />
         <div className='text-white text-center'>
           <h1 className='text-xl font-bold'>Mas io Airdrop</h1>
-          {/* <div className='text-center text-sm font-semibold flex items-center justify-center gap-4 flex-wrap'>
-            <h2>Mini App</h2>
-            <h2>Depin</h2>
-            <h2>Tesnet</h2>
-            <h2>Extention</h2>
-          </div> */}
         </div>
       </div>
     </div>
